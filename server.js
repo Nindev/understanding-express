34280470b7d87b35e16d69094fe60f17c5f021a4
DSallee <dsallee.dev@gmail.com>
2018-09-12 23:43:52 +0200

    Getting content from a json file & ship page added to index page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const data = require('./data/ships.json');
 const app = express();
 const path = require('path');
 // Set the template engine to be pug:
@@ -14,53 +15,37 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-// Render a pug page (views/index.pug)
-// app.get('/', (req, res) => {
-//   res.render('index')
-// })
-
-// Redirect users to a desired ship page
-// if they navigate to the root of the website
-app.get('/', (req,res) =>{
-  res.redirect('/asp-explorer')
+// Render the index pug page (views/index.pug)
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'SHIPYARD STORE',
+    subtitle: 'TAKE TO THE STARS',
+    content: "With real-world physics and credible futuristic engineering, Elite Dangerous' ships are built to feel real. Pick a spacecraft and customize it with hundreds of modular components to complement your play style in a game where your ship and your skills are all that stands between you and the cutthroat galaxy.",
+    bgImage: 'https://media.playstation.com/is/image/SCEA/elite-dangerous-boxart-01-ps4-us-16June2017?$MediaCarousel_Original$'
+  })
 })
 
-// Render each ships on the same pug template
+// Redirect users to the desired ship page
+// if they navigate to the ships link
+app.get('/ships/', (req,res) => {
+  res.redirect('/ships/asp-explorer')
+})
+
+// Render each ships pages on the same pug template
 // Because each ship pages as the same structure
-app.get('/asp-explorer', (req, res) => {
-  res.render('index', {
-    ship: 'ASP EXPLORER',
-    manufacturer: 'Lakon Spaceways',
-    content: 'The Asp Explorer is the civilian version of the military model Asp MkII (which first saw service in 2878). Lakon Spaceways now owns the licence to construct these ships and has marketed them heavily at customers looking for their first multi-crewed ships. The ship class has earned a solid reputation for long-range missions and those requiring some discretion.',
-    crew: '2',
-    mass: '280',
-    price: '6,661,150'
-  })
+app.get('/ships/asp-explorer', (req, res) => {
+  res.render('ships', data.ships[0])
 })
 
-app.get('/imperial-clipper', (req, res) => {
-  res.render('index', {
-    ship: 'IMPERIAL CLIPPER',
-    manufacturer: 'Gutamaya',
-    content: 'The Imperial Clipper is the quintessential Imperial ship. It epitomises elegance of form while delivering speed and agility. Because of its balance of speed, luxury and strength it is used for transporting valuable items and personnel of importance across the Empire. With the navy ships these can come as standard, and not available for civilian, or export models.',
-    crew: '2',
-    mass: '400',
-    price: '22,296,860'
-  })
+app.get('/ships/imperial-clipper', (req, res) => {
+  res.render('ships', data.ships[1])
 })
 
-app.get('/krait-mk2', (req, res) => {
-  res.render('index', {
-    ship: 'KRAIT MKII',
-    manufacturer: 'Faulcon deLacy',
-    content: 'The Krait MkII is a reimagining of the Krait Lightspeeder, which was originally manufactured by Faulcon DeLacy in the 3100s. Although the new ship is larger than the original, it possesses many of the same characteristics, emphasising speed, manoeuvrability and firepower over defensive capability. The ship also boasts a fighter bay and space for up to two crew members, making it a good option for those seeking a medium-weight, multipurpose vessel.',
-    crew: '3',
-    mass: '320',
-    price: '45,814,205'
-  })
+app.get('/ships/krait-mk2', (req, res) => {
+  res.render('ships', data.ships[2])
 })
-////////////////////////////////////////////////
 
+// Listen to server on port 5656 if it's not set in the terminal
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
